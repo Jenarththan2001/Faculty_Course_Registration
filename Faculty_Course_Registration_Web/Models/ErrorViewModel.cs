6fcfe9f78b975c06dc2b24973f67ef89bee04cb7
Akilan Jenarththan <jenarththanakilan@gmail.com>
2025-02-10 14:19:56 +0530

    Add project files.

---
+++ Faculty_Course_Registration_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Faculty_Course_Registration_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
